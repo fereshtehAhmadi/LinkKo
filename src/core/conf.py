54396d1d446e91e0fb964b64b0439dfc89532ef9
fereshteh <freshtehahmadi01@gmail.com>
2026-06-26 18:22:54 +0330

    config database

--- src/core/conf.py
+++ src/core/conf.py
@@ -1,8 +1,15 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    DATABASE_URL: str = "sqlite+aiosqlite:///./app.db"
+    DB_HOST: str = "_"
+    DB_PORT: int = 3306
+    DB_NAME: str = "_"
+    DB_PASS: str = "_"
+
     APP_NAME: str = "LinkKo"
     APP_HOST: str = "127.0.0.1"
     APP_PORT: int = 8000
---
+++ src/database.py
@@ -0,0 +1,35 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import declarative_base
+from src.core.config import settings
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=settings.DEBUG,
+    future=True,
+    pool_pre_ping=True,
+    pool_size=20,
+    max_overflow=40
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+    autocommit=False
+)
+
+Base = declarative_base()
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
